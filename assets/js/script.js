fa9117dcba099ce338d7e609205d297ecee6ec73
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2025-07-26 13:07:40 +0545

    structure and style testimonials section

--- assets/js/script.js
+++ assets/js/script.js
@@ -126,3 +126,18 @@ portfolioCardsWithModals.forEach((portfolioCardWithModal) => {
                     });
 
             });
+
+// testimonials swipper
+var swiper = new Swiper(".sue-client-swiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
